96f764f0802c5b095a274e71f4d56a599ecda98a
Beekenstein <bkelly0905@gmail.com>
2023-07-10 17:36:15 -0400

    changing name to match the project, added notes

--- CodinPracticing/src/main/java/DiceCheck.java
+++ CodinPracticing/src/main/java/DiceCheck.java
@@ -6,6 +6,19 @@ import java.util.*;
  **/
 class Solution {
 
+    /*
+    You are a DM and your players have a lot of dice rolls to perform.
+    A 20-faced-die roll can be considered as:
+    -a Criticall success is 20
+    -a Critical failure is exactly 1
+    in not the outcome is:
+    - a success if it is above the expected value (also called DC) or equal to it
+    - a failure if it is below the expected value
+    You decide to write a script given the rolls will automatically tell you their outcomes, Success,
+    Failure, Critical Success, or Critical Failure.
+    Failure, Critical Success, or Critical Failure.
+     */
+
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int nbRoll = in.nextInt();
@@ -24,6 +37,6 @@ class Solution {
                 System.out.println("Failure");
             }
         }
-        
+
     }
 }
