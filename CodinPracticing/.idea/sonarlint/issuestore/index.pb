8595677ed8c71552699a5931f48c6395c0d32389
Brian Kelly <bkelly0905@gmail.com>
2025-11-04 19:24:05 -0500

    LinkedList

---
+++ CodinPracticing/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,23 @@
+
+Q
+!src/main/java/DuplicateZeros.java,7\6\76e22d82d60cf38e3115aee1bfcc8fcd39c189b6
+M
+src/main/java/PrefixFind.java,3\4\340719aed5cfcd4fe07b9073526a087cb1be13f8
+P
+ src/main/java/EvenArrayInts.java,b\0\b001b8e1a78cc01bb3c347d9f5953fbf5497d5c2
+U
+%src/main/java/InterviewPractice1.java,3\1\311ddb5d315a40c253c8b1d3536a274644dcbb07
+L
+src/main/java/DiceCheck.java,e\b\eb676f21bb94872f721808bb4f35811954092db3
+P
+ src/main/java/DVDCollection.java,3\a\3ad4a08388284e4500f23f7d97ebbaf79c360cfc
+O
+src/main/java/FindExponent.java,1\5\15fad5a2745db975fcce0dea371b498402448bd9
+K
+src/main/java/FizzBuzz.java,a\2\a23da256c5753489825f3b986a5099d6a6d52530
+R
+"src/main/java/CountOccurrence.java,9\7\977f94ea0bdbe46f21c01ce92f4cb47379473d4f
+K
+src/main/java/HexaProf.java,d\c\dc6fa5e70cd4bedbba8fd1ee7e9bed80ec7dc9a2
+G
+src/main/java/test.java,e\2\e2888c50ee409b9c9db52af1582e0e64fc2ac37c
--- CodinPracticing/src/main/java/HexaProf.java
+++ CodinPracticing/src/main/java/HexaProf.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
- class HexaProf {
+class HexaProf {
 
 
     public  static List<String> hexProf(){
@@ -14,13 +16,34 @@ import java.util.List;
          return newList;
     }
 
+     public static int sumOfUnique(int[] nums) {
+
+         Map<Integer, Integer> countMap = new HashMap<>();
+
+         for (int n : nums) {
+             countMap.put(n, countMap.getOrDefault(n, 0) + 1);
+         }
+
+         int sum = 0;
+         for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+             if (entry.getValue() == 1) {
+                 sum += entry.getKey();
+             }
+         }
+         return sum;
+     }
+
     public static void main(String[] args) {
 
         List answer = new ArrayList<>();
                 answer = hexProf();
 
         System.out.println("Count = " + answer);
+        System.out.println(sumOfUnique(new int[]{1, 2, 3, 4, 5, 5}));       // 10
+        System.out.println(sumOfUnique(new int[]{9, 15, 12, 9, 10, 15, 9})); // 22
+        System.out.println(sumOfUnique(new int[]{1, 2, 1, 2}));              // 0
     }
+
 }
 
 
